d26997842a0baa7c937e3eb1dfdc0f1b57891758
gAzraelc <68873524+gAzraelc@users.noreply.github.com>
2023-03-09 16:48:47 +0100

    Add files via upload

---
+++ kali-setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+# Check if this script is running as sudo
+if [ "$EUID" -ne 0 ]
+    then echo "[-] Please run this script with sudo"
+    exit
+fi
+
+# Update repositories list
+sudo apt update
+
+# install most used software
+programs = "gdb python3-pwntools armitage gobuster htop rlwrap zaproxy peass seclists"
+
+sudo apt install $programs
+
+# Update searchsploit
+searchsploit -u
+
+# download most used software repositories
+git clone https://github.com/pwndbg/pwndbg.git
+
+git clone https://github.com/SNGWN/Burp-Suite.git
